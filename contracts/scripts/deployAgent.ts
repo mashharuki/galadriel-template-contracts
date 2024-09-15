e4e3c67422bfcf61af14be609dc8b10c499fe618
Haruki Kondo <avp.104.106.107@ae.auone-net.jp>
2024-09-15 10:43:09 +0000

    更新

---
+++ contracts/scripts/deployAgent.ts
@@ -0,0 +1,30 @@
+import {ethers} from "hardhat";
+
+const AGENT_PROMPT = "You are a helpful assistant";
+
+async function main() {
+  if (!process.env.ORACLE_ADDRESS) {
+    throw new Error("ORACLE_ADDRESS env variable is not set.");
+  }
+  const oracleAddress: string = process.env.ORACLE_ADDRESS;
+  await deployAgent(oracleAddress);
+}
+
+/**
+ * Agentコントラクトをデプロイする。
+ * @param oracleAddress 
+ */
+async function deployAgent(oracleAddress: string) {
+  const agent = await ethers.deployContract("Agent", [oracleAddress, AGENT_PROMPT], {});
+
+  await agent.waitForDeployment();
+
+  console.log(`Agent contract deployed to ${agent.target}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
